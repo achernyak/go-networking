8e4d6ab8b806609fdffad02af8ae2fb3b1564694
Artem Chernyak <artemchernyak@gmail.com>
2017-03-12 19:11:57 -0500

    proxy get

---
+++ ProxyGet/ProxyGet.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: ", os.Args[0],
+			"http://proxy-host:port http://host:port/page")
+		os.Exit(1)
+	}
+	proxyURL, err := url.Parse(os.Args[1])
+	checkError(err)
+	url, err := url.Parse(os.Args[2])
+	checkError(err)
+
+	transport := &http.Transport{Proxy: http.ProxyURL(proxyURL)}
+	client := &http.Client{Transport: transport}
+
+	request, err := http.NewRequest("GET", url.String(), nil)
+
+	dump, _ := httputil.DumpRequest(request, false)
+	fmt.Println(string(dump))
+
+	response, err := client.Do(request)
+	checkError(err)
+	fmt.Println("Read ok")
+
+	if response.Status != "200 OK" {
+		fmt.Println(response.Status)
+		os.Exit(2)
+	}
+
+	chSet := getCharset(response)
+	fmt.Printf("got charset %s\n", chSet)
+	if chSet != "UTF-8" {
+		fmt.Println("Cannot handle", chSet)
+		os.Exit(4)
+	}
+
+	var buf [512]byte
+	reader := response.Body
+	fmt.Println("got body")
+	for {
+		n, err := reader.Read(buf[0:])
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Print(string(buf[0:n]))
+	}
+}
+
+func getCharset(response *http.Response) string {
+	contentType := response.Header.Get("Content-Type")
+	if contentType == "" {
+		return "UTF-08"
+	}
+	idx := strings.Index(contentType, "charset:")
+	if idx == -1 {
+		return "UTF-8"
+	}
+	return strings.Trim(contentType[idx:], " ")
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error", err.Error())
+		os.Exit(1)
+	}
+}
